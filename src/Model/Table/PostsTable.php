9ed8e93891d77d96892f7fa79f9799876fa5a929
Miguel Reboiro-Jato <mrjato@uvigo.es>
2020-10-24 18:48:38 +0200

    Adds post validation
    The fields of the posts now have some restrictions that are checked
    before storing the entities.

--- src/Model/Table/PostsTable.php
+++ src/Model/Table/PostsTable.php
@@ -3,9 +3,20 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class PostsTable extends Table
 {
-}
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->minLength('title', 3)
+            ->maxLength('title', 255)
 
+            ->notEmptyString('content')
+            ->maxLength('content', 255);
+
+        return $validator;
+    }
+}
 ?>
